364d2c71c1924e1ddfe9cd3a3f8fc7e6fd6b14b0
Hamzza <hk151817@gmail.com>
2024-05-26 21:12:07 +0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from 'daisyui';
 
 const config: Config = {
   content: [
@@ -6,15 +7,23 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  daisyui: {
+      themes: [
+        {
+          mytheme: {
+                "primary": "#00c4dc",
+                "secondary": "#e70000",
+                "accent": "#00f28d",
+                "neutral": "#130a11",
+                "base-100": "#39253d",
+                "info": "#007dc0",
+                "success": "#00b02a",
+                "warning": "#b17a00",
+                "error": "#ff6173",
+            },
+          },
+        ],
       },
-    },
-  },
-  plugins: [],
+  plugins: [daisyui],
 };
 export default config;
